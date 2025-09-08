826356a81dc1db88a38bdb1905e31df444b353fe
foster <foster.ogwudu@yahoo.com>
2025-09-08 12:08:27 +0100

    fix noShop

--- src/components/shop/NoShop.tsx
+++ src/components/shop/NoShop.tsx
@@ -28,7 +28,7 @@ function Content() {
   const router = useRouter();
   const { user, isAuthenticated } = useContext(AuthContext);
   const [isOpen, setIsOpen] = useState(false);
-
+  const [openModal, setOpenModal] = useState(false);
   const handleClose = useCallback(() => {
     document.body.classList.remove("overflow-y-hidden");
     setIsOpen(false);
@@ -55,7 +55,12 @@ function Content() {
       </div>
       <div className="flex items-center justify-between">
         {" "}
-        <StartSelling setIsOpen={setIsOpen} />
+        <StartSelling
+          setIsOpen={setIsOpen}
+          openModal={openModal}
+          setOpenModal={setOpenModal}
+          userDetails={user}
+        />
         <div className="flex">
           <button
             type="button"
