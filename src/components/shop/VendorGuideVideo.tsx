bd6feb4ecad1fc30106e3e6daa0003d6ffd80ba5
foster <foster.ogwudu@yahoo.com>
2025-11-07 20:01:02 +0000

    updated guide video

--- src/components/shop/VendorGuideVideo.tsx
+++ src/components/shop/VendorGuideVideo.tsx
@@ -5,7 +5,7 @@ const VendorGuideVideo = () => {
     <div className="w-full max-w-3xl mx-auto aspect-video mb-4">
       <iframe
         className="w-full h-full"
-        src="https://www.youtube.com/embed/eXvxWjAkujc"
+        src="https://www.youtube.com/embed/gguCDZpJQT4"
         title="Vendor Success Story: Jumia Express has impacted our sales positively"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
